7ff3e763fc9ce612edc701850bcc5b09aa89dfb7
Ivaylo Badinov <632282+venimus@users.noreply.github.com>
2019-06-21 16:15:26 +0300

    Code style format (#5)

--- main.tf
+++ main.tf
@@ -14,8 +14,9 @@ resource "aws_route53_record" "cert_validation" {
 }
 
 resource "aws_acm_certificate_validation" "cert" {
-  count   = "${var.validate_cert ? 1: 0}"
+  count           = "${var.validate_cert ? 1: 0}"
   certificate_arn = "${aws_acm_certificate.cert.arn}"
+
   validation_record_fqdns = [
     "${aws_route53_record.cert_validation.fqdn}",
   ]
