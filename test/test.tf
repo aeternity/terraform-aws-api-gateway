61754ba9d8ece9b198ab0dac34d8b96098a8d6d1
Dincho Todorov <dincho@users.noreply.github.com>
2019-07-03 17:43:53 +0300

    Add alternative domain validation to tests

--- test/test.tf
+++ test/test.tf
@@ -58,9 +58,21 @@ resource "aws_route53_record" "cert_validation" {
   ttl     = 60
 }
 
+resource "aws_route53_record" "cert_validation_alt1" {
+  zone_id = "${var.dns_zone}"
+  name    = "${aws_acm_certificate.cert.domain_validation_options.1.resource_record_name}"
+  type    = "${aws_acm_certificate.cert.domain_validation_options.1.resource_record_type}"
+  records = ["${aws_acm_certificate.cert.domain_validation_options.1.resource_record_value}"]
+  ttl     = 60
+}
+
 resource "aws_acm_certificate_validation" "cert" {
-  certificate_arn         = "${aws_acm_certificate.cert.arn}"
-  validation_record_fqdns = ["${aws_route53_record.cert_validation.fqdn}"]
+  certificate_arn = "${aws_acm_certificate.cert.arn}"
+
+  validation_record_fqdns = [
+    "${aws_route53_record.cert_validation_alt1.fqdn}",
+    "${aws_route53_record.cert_validation.fqdn}",
+  ]
 }
 
 module "aws_deploy-test-gw" {
