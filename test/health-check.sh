db5ddc8ee3c126e0b8d82f172c42213131f6ed5a
Michal `acid` Bagrowski <michal@bagrowski.com>
2022-01-25 22:14:27 +0100

    add debug and send som outpu

--- test/health-check.sh
+++ test/health-check.sh
@@ -3,16 +3,18 @@
 set -Eeuo pipefail
 
 API_ADDR=$(terraform output -json |jq -r '."api_gate_fqdn"."value"')
-
+echo "Checking" $API_ADDR
 # Basic health check endpoint
+echo "https://${API_ADDR}/healthz"
 curl -sSf -o /dev/null --retry 10 --retry-connrefused https://${API_ADDR}/healthz
-
+echo "http://${API_ADDR}/v2/status"
 # HTTP -> HTTPS redirect
 curl -sSf -L -o /dev/null --retry 10 --retry-connrefused http://${API_ADDR}/v2/status
-
+echo "https://${API_ADDR}/v2/status"
 # External API
 curl -sSf -o /dev/null --retry 10 --retry-connrefused https://${API_ADDR}/v2/status
-
+echo "https://${API_ADDR}/middleware/status"
+echo "https://${API_ADDR}/mdw/status"
 # Middleware API
 curl -sSf -o /dev/null --retry 10 --retry-connrefused https://${API_ADDR}/middleware/status
 curl -sSf -o /dev/null --retry 10 --retry-connrefused https://${API_ADDR}/mdw/status
