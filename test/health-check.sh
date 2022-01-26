486037f5acb41befa3d33f62d59410a84f5d06f8
Michal `acid` Bagrowski <michal@bagrowski.com>
2022-01-26 20:16:27 +0100

    debug

--- test/health-check.sh
+++ test/health-check.sh
@@ -14,25 +14,25 @@ echo "Checking External API"
 curl -sSf -o /dev/null --retry 10 --retry-connrefused https://${API_ADDR}/v2/status
 echo "Checking Middleware API"
 # Middleware API
-curl -sSf -o /dev/null --retry 10 --retry-connrefused https://${API_ADDR}/middleware/status
-curl -sSf -o /dev/null --retry 10 --retry-connrefused https://${API_ADDR}/mdw/status
+curl -sSf --retry 10 --retry-connrefused https://${API_ADDR}/middleware/status
+curl -sSf --retry 10 --retry-connrefused https://${API_ADDR}/mdw/status
 
 echo "Checking Internal API (dry-run)"
 # Internal API (dry-run)
-EXT_STATUS=$(curl -sS -o /dev/null --retry 10 --retry-connrefused \
+EXT_STATUS=$(curl -sS --retry 10 --retry-connrefused \
     -X POST -H 'Content-type: application/json' -d '{"txs": []}' \
     -w "%{http_code}" \
     https://${API_ADDR}/v2/debug/transactions/dry-run)
 [ $EXT_STATUS -eq 200 ]
 echo "Checking State Channes WebScoket API"
 # State Channels WebSocket API
-WS_STATUS=$(curl -sS -o /dev/null --retry 10 --retry-connrefused \
+WS_STATUS=$(curl -sS --retry 10 --retry-connrefused \
     -w "%{http_code}" \
     https://${API_ADDR}/channel?role=initiator)
 [ $WS_STATUS -eq 426 ]
 echo "Checking Middleware WebSocket API"
 # Middleware WebSocket API
-WS_STATUS=$(curl -sS -I -o /dev/null --retry 10 --retry-connrefused \
+WS_STATUS=$(curl -sS -I --retry 10 --retry-connrefused \
     -w "%{http_code}" \
     https://${API_ADDR}/mdw/websocket)
 [ $WS_STATUS -eq 426 ]
