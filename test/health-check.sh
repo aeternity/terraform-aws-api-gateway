1469f618c730efa7e6b9f6c2669a45c43c5eb9cc
Michal `acid` Bagrowski <michal@bagrowski.com>
2022-01-26 07:47:40 +0100

    add some messages CI/CD failing because no output

--- test/health-check.sh
+++ test/health-check.sh
@@ -6,31 +6,31 @@ API_ADDR=$(terraform output -json |jq -r '."api_gate_fqdn"."value"')
 echo "Checking" $API_ADDR
 # Basic health check endpoint
 curl -sSf -o /dev/null --retry 10 --retry-connrefused https://${API_ADDR}/healthz
-
+echo "Checking HTTP -> HTTPS redirect"
 # HTTP -> HTTPS redirect
 curl -sSf -L -o /dev/null --retry 10 --retry-connrefused http://${API_ADDR}/v2/status
-
+echo "Checking External API"
 # External API
 curl -sSf -o /dev/null --retry 10 --retry-connrefused https://${API_ADDR}/v2/status
-
+echo "Checking Middleware API"
 # Middleware API
 curl -sSf -o /dev/null --retry 10 --retry-connrefused https://${API_ADDR}/middleware/status
 curl -sSf -o /dev/null --retry 10 --retry-connrefused https://${API_ADDR}/mdw/status
 
-
+echo "Checking Internal API (dry-run)"
 # Internal API (dry-run)
 EXT_STATUS=$(curl -sS -o /dev/null --retry 10 --retry-connrefused \
     -X POST -H 'Content-type: application/json' -d '{"txs": []}' \
     -w "%{http_code}" \
     https://${API_ADDR}/v2/debug/transactions/dry-run)
 [ $EXT_STATUS -eq 200 ]
-
+echo "Checking State Channes WebScoket API"
 # State Channels WebSocket API
 WS_STATUS=$(curl -sS -o /dev/null --retry 10 --retry-connrefused \
     -w "%{http_code}" \
     https://${API_ADDR}/channel?role=initiator)
 [ $WS_STATUS -eq 426 ]
-
+echo "Checking Middleware WebSocket API"
 # Middleware WebSocket API
 WS_STATUS=$(curl -sS -I -o /dev/null --retry 10 --retry-connrefused \
     -w "%{http_code}" \
