a06c9f21cca99207a54ac5510efc2bcf5e34f5c0
Michal `acid` Bagrowski <michal@bagrowski.com>
2022-02-01 22:27:54 +0100

    fix vert new tf

--- test/cert.tf
+++ test/cert.tf
@@ -4,26 +4,31 @@ resource "aws_acm_certificate" "cert" {
   validation_method         = "DNS"
 }
 
-resource "aws_route53_record" "cert_validation" {
-  zone_id = var.dns_zone
-  name    = aws_acm_certificate.cert.domain_validation_options.0.resource_record_name
-  type    = aws_acm_certificate.cert.domain_validation_options.0.resource_record_type
-  records = [aws_acm_certificate.cert.domain_validation_options.0.resource_record_value]
-  ttl     = 60
-}
 
-resource "aws_route53_record" "cert_validation_alt1" {
-  zone_id = var.dns_zone
-  name    = aws_acm_certificate.cert.domain_validation_options.1.resource_record_name
-  type    = aws_acm_certificate.cert.domain_validation_options.1.resource_record_type
-  records = [aws_acm_certificate.cert.domain_validation_options.1.resource_record_value]
-  ttl     = 60
+resource "aws_route53_record" "cert_validation" {
+  for_each = {
+    for dvo in aws_acm_certificate.cert.domain_validation_options : dvo.domain_name => {
+      name   = dvo.resource_record_name
+      record = dvo.resource_record_value
+      type   = dvo.resource_record_type
+    }
+  }
+
+  allow_overwrite = true
+  name            = each.value.name
+  records         = [each.value.record]
+  ttl             = 60
+  type            = each.value.type
+  zone_id         = var.ops_aeternity_com_zone_id
 }
 
 resource "aws_acm_certificate_validation" "cert" {
   certificate_arn = aws_acm_certificate.cert.arn
-  validation_record_fqdns = [
-    aws_route53_record.cert_validation.fqdn,
-    aws_route53_record.cert_validation_alt1.fqdn
-  ]
+  validation_record_fqdns = concat(
+    for record in aws_route53_record.cert_validation : record.fqdn,
+    for record in aws_route53_record.cert_validation_alt1 : record.fqdn)
+  )
+
+
+
 }
