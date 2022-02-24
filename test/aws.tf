eae0c41d5456b86660806c65e9408bfaa5116483
Michał <github@bagrowski.com>
2022-02-24 21:03:24 +0100

    bump aws provider (#38)
    * bump aws provider

--- test/aws.tf
+++ test/aws.tf
@@ -1,23 +1,19 @@
 # Default
 provider "aws" {
-  version = "2.33.0"
-  region  = "us-east-1"
+  region = "us-east-1"
 }
 
 provider "aws" {
-  version = "2.33.0"
-  region  = "ap-southeast-2"
-  alias   = "ap-southeast-2"
+  region = "ap-southeast-2"
+  alias  = "ap-southeast-2"
 }
 
 provider "aws" {
-  version = "2.33.0"
-  region  = "ap-southeast-2"
-  alias   = "lb1"
+  region = "ap-southeast-2"
+  alias  = "lb1"
 }
 
 provider "aws" {
-  version = "2.33.0"
-  region  = "eu-west-2"
-  alias   = "eu-west-2"
+  region = "eu-west-2"
+  alias  = "eu-west-2"
 }
